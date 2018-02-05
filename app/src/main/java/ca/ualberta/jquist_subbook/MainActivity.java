4b2ac757d56b67da97897f0f2dc4c126f327f8cb
Jonah Quist <jonahquist@gmail.com>
2018-02-05 12:49:12 -0700

    Added edit activity

--- app/src/main/java/ca/ualberta/jquist_subbook/MainActivity.java
+++ app/src/main/java/ca/ualberta/jquist_subbook/MainActivity.java
@@ -1,5 +1,6 @@
 package ca.ualberta.jquist_subbook;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -34,11 +35,11 @@ public class MainActivity extends AppCompatActivity {
         list.setAdapter(subAdapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final Intent editIntent = new Intent(this, EditActivity.class);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(editIntent);
             }
         });
     }
