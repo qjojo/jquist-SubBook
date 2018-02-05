186c2874c55fae34c27c3e934dfc6f7a29f9e388
Jonah Quist <jonahquist@gmail.com>
2018-02-05 12:54:33 -0700

    EditActivity now reachable

--- app/src/main/java/ca/ualberta/jquist_subbook/EditActivity.java
+++ app/src/main/java/ca/ualberta/jquist_subbook/EditActivity.java
@@ -2,6 +2,8 @@ package ca.ualberta.jquist_subbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EditActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class EditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        Button saveButton = findViewById(R.id.saveB);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        Button delButton = findViewById(R.id.delB);
+        delButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
